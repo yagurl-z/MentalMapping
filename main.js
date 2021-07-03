d06e74729aecac4e45b0f469bfd778eff4f347f2
yagurl-z <52004558+yagurl-z@users.noreply.github.com>
2021-07-02 20:34:34 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "215px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
